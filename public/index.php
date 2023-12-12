72ae077a8b29d4df8aef817c33e38ae82c81a4eb
Min Phone Aung <88699874+PeterHyperAung@users.noreply.github.com>
2023-12-12 23:39:20 +0630

    modles and form validation

--- public/index.php
+++ public/index.php
@@ -2,13 +2,17 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 use app\core\Application;
+use app\controllers\SiteController;
+use app\controllers\AuthController;
 
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'index');
-
 $app->router->get('/contact', 'contact'); 
-
-$app->router->post('/contact', [\app\controllers\SiteController::class, 'handleContact']);
-
+$app->router->post('/contact', [SiteController::class, 'handleContact']);
+$app->router->get("/login", [AuthController::class, 'login']);
+$app->router->post("/login", [AuthController::class, 'login']);
+$app->router->get("/register", [AuthController::class, 'register']);
+$app->router->post("/register", [AuthController::class, 'register']);
+ 
 $app->run();
